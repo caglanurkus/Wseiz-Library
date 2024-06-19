e3e799b7c5bd1e2a08f7e6c8af22e22f9bbe0cce
Cagla <caglanurkuss@gmail.com>
2024-06-19 12:48:27 +0200

    book

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
 
+
 class Book:
     def __init__(self, title, author, isbn):
         self.title = title
@@ -7,4 +8,4 @@ class Book:
         self.available = True
 
     def __str__(self):
-        return f"Book(Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Available: {self.available})" 
+        return f"Book(Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Available: {self.available})"  
